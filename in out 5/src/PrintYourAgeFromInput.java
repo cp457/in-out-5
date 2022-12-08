2707a5b1d13c684753806ecc0c0ec30408de74b7
cp457 <112082288+cp457@users.noreply.github.com>
2022-12-08 15:06:46 +0100

    Add files via upload

---
+++ in out 5/src/PrintYourAgeFromInput.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class PrintYourAgeFromInput {
+    public static void main(String[] args) {
+
+
+
+
+        System.out.println("Put your age");
+        Scanner input = new Scanner(System.in);
+
+        int age = input.nextInt();
+
+
+
+        System.out.printf("Your age is: `%d` ", age);
+        input.close();
+
+
+    }
+
+}
+
+/* // task 1: remove this comment and write one line of code here
+
+int age = input.nextInt(); // read the integer number input from user
+
+// task 2: remove this comment and write one line of code here
+
+input.close(); // close the scanner (do not read anymore the user input)
+
+* **task 1**: remove the comment and print in console `Enter your age: `
+* **task 2**: remove the comment and print in console `Your age is: ` using the `%d` *converter* for printing the `age` variable's value
+* the expected output will be divided in 3 steps:
+  * `Enter your age: ` and the Java program waits for the *user input* (that is your input)
+  * then, you write down your age directly in the console (e.g. `25`): `Enter your age: 25`
+  * the Java program takes your input with the Scanner and prints your age: `Your age is: 25`
+* so the final expected output will be something like this:
+
+Enter your age: 25 Your age is: 25 */
